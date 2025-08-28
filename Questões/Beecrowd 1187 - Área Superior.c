1d672eadad40f8f70d4bea22b00c0df66a010142
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2025-08-28 08:47:46 -0300

    Create Beecrowd 1187 - Área Superior.c

---
+++ Questões/Beecrowd 1187 - Área Superior.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+  char operacao;
+  double m[12][12], soma = 0, media = 0;
+
+  scanf(" %c", &operacao);
+
+  for(int i = 0; i < 12; i++){
+    for(int j = 0; j < 12; j++){
+      scanf("%lf", &m[i][j]);
+      if(j > i && (i + j) < 11){
+        soma += m[i][j];
+      }
+    }
+  }
+
+   operacao = toupper(operacao);
+
+   switch(operacao){
+     case 'S': printf("%.1lf\n", soma); break;
+     case 'M': media = soma / 30; printf("%.1lf\n", media); break;
+     default: break;
+   }
+
+  return 0;
+}
