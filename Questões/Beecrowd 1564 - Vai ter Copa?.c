2187cae5f45baba989cc59b3865726a4f1e9073a
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2025-09-05 09:17:32 -0300

    Create Beecrowd 1564 - Vai ter Copa?.c

---
+++ Questões/Beecrowd 1564 - Vai ter Copa?.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+ int vaiterounao;
+
+  while(scanf("%d", &vaiterounao) != EOF){
+    if(vaiterounao == 0)
+      printf("vai ter copa!\n");
+    
+    else
+        printf("vai ter duas!\n");
+    }
+  
+  return 0;
+}
