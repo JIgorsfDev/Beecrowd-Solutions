ccbcc70d083291f6930472cb7560a1aeb0546fa8
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2025-09-05 09:16:22 -0300

    Create Beecrowd 1759 - Ho Ho Ho.c

---
+++ Questões/Beecrowd 1759 - Ho Ho Ho.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+   int hohoho, aux = 0;
+
+   scanf("%d", &hohoho);
+
+   while(aux != hohoho - 1){
+    printf("Ho ");
+    aux++;
+   }
+    printf("Ho!\n");
+
+return 0;
+}
